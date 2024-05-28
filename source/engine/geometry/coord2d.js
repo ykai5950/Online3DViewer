20029112b99f52d846617820deb9a11bb9861acf
SLVDI138\yuichi.kai <yuichi.kai@sola-air.com>
2024-05-28 18:50:03 +0900

    3次元平面を2次元に投影して面積を求めるメソッド実装

--- source/engine/geometry/coord2d.js
+++ source/engine/geometry/coord2d.js
@@ -15,6 +15,28 @@ export class Coord2D {
 	}
 }
 
+// 2次元に射影した3次元座標で囲まれた面積を求めるメソッド
+export function Area(points) {
+	// 3点以上ない場合
+	if (points.length < 3) {
+		return 0;
+	}
+
+	// 面積を格納する変数を定義
+	let area = 0;
+
+	// 面積を加算
+	for (let i = 0; i < points.length; i++) {
+		if (i < points.length - 1) {
+			area += (points[i].x - points[i + 1].x) * (points[i].y + points[i + 1].y);
+		} else {
+			area += (points[i].x - points[0].x) * (points[i].y + points[0].y);
+		}
+	}
+
+	return area / 2;
+}
+
 // 2つのCoord2Dオブジェクトが等しいかどうかを判定する関数
 export function CoordIsEqual2D(a, b) {
 	// 2つのCoord2Dオブジェクトのx座標とy座標が等しいかどうかを判定する
